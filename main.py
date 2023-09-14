da56efd3d82d53dff8f22f5097dbb0c250552181
Yabushita111 <anedtost@yahoo.co.jp>
2023-09-14 17:53:17 +0900

    a

--- main.py
+++ main.py
@@ -7,6 +7,14 @@
 #
 # This file can be a nice home for your Battlesnake logic and helper functions.
 #
+
+
+
+
+
+
+
+
 # To get you started we've included code to prevent your Battlesnake from moving backwards.
 # For more info see docs.battlesnake.com
 
